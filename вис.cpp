cd555a9fe24895260cd2f13f80903b2e53bcb66d
ReshetnikovaGolyakovaIT1516 <golakovak3@gmail.com>
2024-12-10 18:41:03 +0500

    // Функция для отображения состояния игры

--- вис.cpp
+++ вис.cpp
@@ -4,3 +4,28 @@
 #include <ctime>   
 #include "Windows.h"
 using namespace std;
+// Функция для отображения состояния игры
+void displayState(const string& word, const string& guessed)
+{
+    for (char c : word)
+    {
+        bool found = false;
+        for (char g : guessed)
+        {
+            if (c == g)
+            {
+                found = true; // Буква найдена среди угаданных
+                break;
+            }
+        }
+        if (found)
+        {
+            cout << c << " "; // Если буква угадана, показываем её
+        }
+        else
+        {
+            cout << "_ ";
+        }
+    }
+    cout << endl;
+}
